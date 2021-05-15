1d170b84fb0dfd51b5ea178ae07316923c27fff6
Ali Mahzoon <ali.mahzoon.1989@gmail.com>
2021-05-14 23:49:56 -0400

    Capitalized Edited

--- Python Solutions/Basic Python/11. Capitalize.py
+++ Python Solutions/Basic Python/11. Capitalize.py
@@ -14,3 +14,21 @@ if __name__ == '__main__':
     fptr.write(result + '\n')
 
     fptr.close()
+
+# OR
+
+def solve(s):
+    
+    for x in s[:].split():
+        s =s.replace(x, x.capitalize())
+    return s
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
