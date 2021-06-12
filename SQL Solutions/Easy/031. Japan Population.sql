260a892b2bd9820088a71e3107e271e1dee8815e
Ali Mahzoon <ali.mahzoon.1989@gmail.com>
2021-06-11 22:26:50 -0400

    Japan Population

---
+++ SQL Solutions/Easy/031. Japan Population.sql
@@ -0,0 +1,4 @@
+-- Problem : https://www.hackerrank.com/challenges/japan-population/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+select sum(population) from city where countrycode = 'JPN';
