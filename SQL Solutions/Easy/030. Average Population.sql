2bfa544d5042aba3281c2624bd238fd4b8c26ca9
Ali Mahzoon <ali.mahzoon.1989@gmail.com>
2021-06-11 22:26:09 -0400

    Average Population

---
+++ SQL Solutions/Easy/030. Average Population.sql
@@ -0,0 +1,4 @@
+-- Problem: https://www.hackerrank.com/challenges/average-population/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+select round(avg(population)) from city;
