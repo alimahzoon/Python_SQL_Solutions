c359df937080d96c4b5be7c2ca41dafadc47cef5
Ali Mahzoon <ali.mahzoon.1989@gmail.com>
2021-06-11 22:27:38 -0400

    Population Density Difference

---
+++ SQL Solutions/Easy/032. Population Density Difference.sql
@@ -0,0 +1,4 @@
+-- Problem: https://www.hackerrank.com/challenges/population-density-difference/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+select max(population) - min(population) from city;
