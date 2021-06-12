6c6f9549f032cd12df10ea26955822c75a8bac03
Ali Mahzoon <ali.mahzoon.1989@gmail.com>
2021-06-11 22:25:38 -0400

    Revising Aggregations - Averages

---
+++ SQL Solutions/Easy/029. Revising Aggregations - Averages.sql
@@ -0,0 +1,5 @@
+-- Problem: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+
+select avg(population) from city where district = 'California';
