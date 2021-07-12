2954843fae40aa391fb6cbb76c90820e7e878dbd
Ali Mahzoon <ali.mahzoon.1989@gmail.com>
2021-07-11 21:21:40 -0400

    If Else

---
+++ Python Solutions/Basic Python/01. Python If-Else.py
@@ -0,0 +1,6 @@
+# Problem: https://www.hackerrank.com/challenges/py-if-else/problem
+
+n = int(input().strip())
+check = {True: "Not Weird", False: "Weird"}
+
+print(check[n%2==0 and (n in range(2,6) or n > 20)])
